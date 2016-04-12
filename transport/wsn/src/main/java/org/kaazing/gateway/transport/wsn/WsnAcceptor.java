05585c41e6422f27f7e20565f69f2a0a6dfe8a44
David Witherspoon <david.witherspoon@kaazing.com>
2016-04-12 13:17:42 -0700

    Added back verify application challenge scheme

--- transport/wsn/src/main/java/org/kaazing/gateway/transport/wsn/WsnAcceptor.java
+++ transport/wsn/src/main/java/org/kaazing/gateway/transport/wsn/WsnAcceptor.java
@@ -1077,9 +1077,9 @@ public class WsnAcceptor extends AbstractBridgeAcceptor<WsnSession, WsnBindings.
                     }
 
 //                     If configured with "Application xxx" challenge scheme, reject and close the connection
-//                    if (!verifyApplicationChallengeSchemeSecurity(session)) {
-//                        return;
-//                    }
+                    if (!verifyApplicationChallengeSchemeSecurity(session)) {
+                        return;
+                    }
 
                     // find (based on this http session) the local address for the WS session
                     // we are about to upgrade to.
