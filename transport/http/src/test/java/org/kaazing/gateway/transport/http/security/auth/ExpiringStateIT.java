227ea904ea7e44b6e86be5175861d0ad0f0eac52
apirvu <alex.pirvu.luxoft@gmail.com>
2016-09-14 06:26:33 -0700

    Updated GCB to start 2 Gw in cluster + small changes in scrips

--- transport/http/src/test/java/org/kaazing/gateway/transport/http/security/auth/ExpiringStateIT.java
+++ transport/http/src/test/java/org/kaazing/gateway/transport/http/security/auth/ExpiringStateIT.java
@@ -13,24 +13,27 @@ import org.kaazing.gateway.util.feature.EarlyAccessFeatures;
 import org.kaazing.k3po.junit.annotation.Specification;
 import org.kaazing.k3po.junit.rules.K3poRule;
 
-
+import java.util.concurrent.TimeUnit;
 
 import org.kaazing.test.util.ITUtil;
 import org.kaazing.test.util.ResolutionTestUtils;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.kaazing.test.util.ITUtil.createRuleChain;
+
+
+
+
 
 
 
 public class ExpiringStateIT {
 
 
+
     private final K3poRule k3po = new K3poRule();
 
     private static String networkInterface = ResolutionTestUtils.getLoopbackInterface();
 
-    private GatewayClusterRule rule = new GatewayClusterRule() {
+    private GatewayClusterRule gwRule = new GatewayClusterRule() {
         {
 
             String clusterMember1URI = "tcp://localhost:8081";
@@ -39,50 +42,46 @@ public class ExpiringStateIT {
             String clusterMember2URIcl = "tcp://localhost:5942";
 
 
-//            GatewayConfiguration config1 = createGatewayConfigBuilder(
-//                    clusterMember1URI, clusterMember2URI, "7001", "8000");
-//            GatewayConfiguration config2 = createGatewayConfigBuilder(
-//                    clusterMember2URI, clusterMember1URI, "7000", "8001");
+            GatewayConfiguration config1 = createGatewayConfigBuilder(clusterMember1URIcl, clusterMember2URIcl, clusterMember1URI);
+            GatewayConfiguration config2 = createGatewayConfigBuilder(clusterMember2URIcl, clusterMember1URIcl, clusterMember2URI);
 
-    private GatewayConfiguration createGatewayConfigBuilder(String clusterMember1URI,
-                                                            String clusterMember2URI, String servicePort) {
-        String balancerURI1 = "ws://gateway.example.com:8001";
+            init(config1, config2);
+        }
+    };
+
+
+    private GatewayConfiguration createGatewayConfigBuilder(String acceptClusterLink,
+                                                            String connectClusterLink,
+                                                            String acceptMemberLink) {
         return new GatewayConfigurationBuilder()
+                .property("login.module.expiring.state", "true")
                 .cluster()
-                  .accept(clusterMember1URIcl)
-                  .connect(clusterMember2URIcl)
+                  .accept(acceptClusterLink)
+                  .connect(connectClusterLink)
                   .name("clusterName")
                 .done()
                 .service()
                   .type("directory")
-                  .accept("tcp://localhost:" + servicePort)
-                  .accept(clusterMember1URI)
-                  .accept(clusterMember2URI)
+                  .accept("tcp://localhost:8080")
+                  .accept(acceptMemberLink)
                 .done()
                 .security()
-                  .trustStore(trustStore)
-                  .keyStore(keyStore)
-                  .keyStorePassword(password)
-                  .keyStoreFile(getKeystoreFileLocation())
                   .realm()
                     .name("demo")
                     .description("Kaazing WebSocket Gateway Demo")
                     .httpChallengeScheme("Basic")
+                    .done()
+                  .done()
 //                    .httpQueryParameter("token")
-//                .userPrincipalClass("org.kaazing.gateway.management.test.util.TokenCustomLoginModule$RolePrincipal")
-//                .userPrincipalClass("org.kaazing.gateway.management.test.util.TokenCustomLoginModule$UserPrincipal")
 //                .loginModule()
 //                .type("class:org.kaazing.gateway.management.test.util.TokenCustomLoginModule")
-                .success("required")
-                .done()
-                .done()
                 .done();
+
     }
 
-
-
     @Rule
-    public TestRule chain = k3po;
+    public TestRule chain = createRuleChain(10, TimeUnit.SECONDS).around(k3po).around(gwRule);
+
 
     @Test
     @Specification({
